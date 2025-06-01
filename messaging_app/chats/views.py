7b802cca9254d290c570e8728394ebccc8c24bfc
Agnes Nduta <47296541+AgnesNM@users.noreply.github.com>
2025-06-01 22:51:44 +0300

    Update views.py

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -711,7 +711,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('chats.urls')),
+    path('api/', include('chats.urls')),
     path('api-auth/', include('rest_framework.urls')),
 ]
 
